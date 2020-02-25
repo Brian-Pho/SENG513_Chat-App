58361cd972ac12ee2d40b5fe04ebee71eda55ef9
Brian Pho <brianpho7@gmail.com>
2020-02-24 20:22:55 -0700

    Clean up front-end boilerplate code

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
